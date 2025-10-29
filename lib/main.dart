f65e04a56574ce8f1819dfe54e0eae0aaeb4be4f
princeampofo <pla252@nyu.edu>
2025-10-29 18:00:16 -0400

    setup with helper code

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+
+void main() {
+  runApp(const SignupAdventureApp());
+}
+
+class SignupAdventureApp extends StatelessWidget {
+  const SignupAdventureApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Adventure ',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Roboto',
+      ),
+      home: const WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
